07434c911b55138ebfc04fdfc1a7b64a982b52f3
Vytenis Bivainis <monrealis@gmail.com>
2019-08-22 00:55:35 +0300

    Add Event DTO

--- src/test/java/shield/JsonParserTest.java
+++ src/test/java/shield/JsonParserTest.java
@@ -9,27 +9,40 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 class JsonParserTest {
 	ObjectMapper mapper = new ObjectMapper();
 
 	@Test
-	void parsesAllEventsFromSample() throws IOException {
-		assertEquals(7, parseEvents().size());
+	void parsesAllEventsFromSample() {
+		List<Event> events = parseEvents();
+
+		assertEquals(7, events.size());
+		assertEquals("profile_create", events.get(0).type);
+		assertEquals("request", events.get(1).type);
 	}
 
 	// https://stackoverflow.com/questions/24835431/use-jackson-to-stream-parse-an-array-of-json-objects
-	private List<?> parseEvents() throws IOException {
-		List<Object> events = new ArrayList<>();
+	private List<Event> parseEvents() {
 		try (JsonParser parser = createParser()) {
+			List<Event> events = new ArrayList<>();
 			while (parser.nextToken() == JsonToken.START_OBJECT)
-				events.add(mapper.readTree(parser));
+				toEvent(events, parser);
+			return events;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
-		return events;
+	}
+
+	private void toEvent(List<Event> events, JsonParser parser) throws IOException {
+		TreeNode node = mapper.readTree(parser);
+		events.add(mapper.convertValue(node, Event.class));
 	}
 
 	private JsonParser createParser() throws IOException, JsonParseException {
@@ -38,5 +51,10 @@ class JsonParserTest {
 
 	private InputStream loadSample() {
 		return getClass().getResourceAsStream("/sample.json");
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	private static class Event {
+		public String type;
 	}
 }
