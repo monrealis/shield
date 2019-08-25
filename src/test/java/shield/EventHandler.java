4c76166e7e5d5f59510ad9e850db3e66e301d883
Vytenis Bivainis <monrealis@gmail.com>
2019-08-25 16:29:02 +0300

    Start emitting output

--- src/test/java/shield/EventHandler.java
+++ src/test/java/shield/EventHandler.java
@@ -1,16 +1,56 @@
 package shield;
 
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class EventHandler {
-	private Map<String, Event> currentPolicy = new HashMap<>();
+	private static ObjectMapper mapper = new ObjectMapper();
+	private final Map<String, Event> currentPolicy = new HashMap<>();
+	private final PrintWriter output;
+
+	public EventHandler(PrintWriter output) {
+		this.output = output;
+		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	}
 
 	public void handle(Event event) {
-		currentPolicy.put(event.modelName, event);
+		if (event.type.equals("profile_create") || event.type.equals("profile_update"))
+			currentPolicy.put(event.modelName, event);
+		else
+			handleRequest(event);
+	}
+
+	private void handleRequest(Event event) {
+		Decision d = new Decision();
+		d.action = "allow";
+		d.requestId = event.requestId;
+		String s = marshall(d);
+		output.println(s);
+	}
+
+	private String marshall(Decision d) {
+		try {
+			String s = mapper.writeValueAsString(d);
+			return s;
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public boolean inspectRequest(String modelName, String url) {
 		return currentPolicy.get(modelName).whitelist.contains(url);
+	}
+
+	private static class Decision {
+		@JsonProperty("request_id")
+		public String requestId;
+		@JsonProperty("action")
+		public String action;
 	}
 }
--- src/test/java/shield/EventHandlerTest.java
+++ src/test/java/shield/EventHandlerTest.java
@@ -2,13 +2,19 @@ package shield;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class EventHandlerTest {
-	private EventHandler handler = new EventHandler();
+	private StringWriter output = new StringWriter();
+	private EventHandler handler = new EventHandler(new PrintWriter(output));
 
 	@Test
 	void allowsOnlyWhitelistedUrls() {
@@ -22,5 +28,30 @@ public class EventHandlerTest {
 
 		assertTrue(handler.inspectRequest("M1", "facebook.com"));
 		assertFalse(handler.inspectRequest("M1", "other.com"));
+	}
+
+	@Test
+	@Disabled
+	void logsRequestToOutput() {
+		Event event = new Event();
+		event.type = "profile_create";
+		event.modelName = "M1";
+		event.defaultPolicy = "block";
+		event.whitelist = asList("facebook.com");
+		event.blacklist = emptyList();
+		handler.handle(event);
+
+		handler.handle(createRequest("r1", "M1", "facebook.com"));
+
+		assertEquals("{'request_id': 'r1', 'action': 'allow'}\n".replace('\'', '"'), output.toString());
+	}
+
+	private Event createRequest(String id, String device, String url) {
+		Event e = new Event();
+		e.type = "request";
+		e.requestId = id;
+		e.deviceId = device;
+		e.url = url;
+		return e;
 	}
 }
